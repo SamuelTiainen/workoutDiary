470b09c0d4fceeca2f4b8ffad9a0297b4ae9e714
SamuelTiainen <127877806+SamuelTiainen@users.noreply.github.com>
2024-10-15 17:52:20 +0300

    components and styles

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createContext, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import addWorkout from './components/addWorkout';
+import workoutHistory from './components/workoutHistory';
+import settings from './components/settings';
+
+const Stack = createStackNavigator();
+export const WorkoutContext = createContext();
 
 export default function App() {
+  const [workouts, setWorkouts] = useState([]);
+  const [unit, setUnit] = useState('km');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WorkoutContext.Provider value={{ workouts, setWorkouts, unit, setUnit }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Add workout" component={addWorkout} />
+          <Stack.Screen name="Workout History" component={workoutHistory} />
+          <Stack.Screen name="Settings" component={settings} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </WorkoutContext.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
